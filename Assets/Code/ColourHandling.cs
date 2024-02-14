cdd29fb7d8906df774b9c8573da0f511ce60d379
shreyapatelcodes <62149620+shreyapatelcodes@users.noreply.github.com>
2024-02-14 15:54:23 -0600

    Fixed color mixing

--- Assets/Code/ColourHandling.cs
+++ Assets/Code/ColourHandling.cs
@@ -5,162 +5,110 @@ using UnityEngine.UI;
 
 public class ColorHandling : MonoBehaviour
 {
-    private int redCount = 0;
-    private int yellowCount = 0;
-    private int blueCount = 0;
-    private int clickCount = 0;
     private int[] lastColor = new int[4];
+    private int lastIndex = 0; 
 
     public SpriteRenderer resultRenderer;
 
     public void IncrementRedCount()
     {
-        redCount++;
-        Debug.Log( redCount + blueCount + yellowCount);
-        if((redCount + yellowCount + blueCount)>4){
-            int oldestColor = lastColor[clickCount%4];
-            Debug.Log("oldest color: " + oldestColor);
-            lastColor[clickCount%4] = 1;
-            if (oldestColor == 1){
-                redCount--;
-            }
-            else if (oldestColor == 2){
-                yellowCount--;
-            }
-            else if (oldestColor == 3){
-                blueCount--;
-            }
-        }
-        clickCount++;
-        Debug.Log("Red count: " + redCount + "Blue count: " + blueCount + "Yellow count: " + yellowCount);
+        UpdateLastColor(1);
         UpdateResultColor();
     }
 
     public void IncrementYellowCount()
     {
-        yellowCount++;
-        if((redCount + yellowCount + blueCount)>4){
-            int oldestColor = lastColor[clickCount%4];
-            Debug.Log("oldest color: " + oldestColor);
-            lastColor[clickCount%4] = 2;
-            if (oldestColor == 1){
-                redCount--;
-            }
-            else if (oldestColor == 2){
-                yellowCount--;
-            }
-            else if (oldestColor == 3){
-                blueCount--;
-            }
-        }
-        clickCount++;
-        Debug.Log("Red count: " + redCount + "Blue count: " + blueCount + "Yellow count: " + yellowCount);
+        UpdateLastColor(2);
         UpdateResultColor();
     }
 
     public void IncrementBlueCount()
     {
-        blueCount++;
-        if((redCount + yellowCount + blueCount)>4){
-            int oldestColor = lastColor[clickCount%4];
-            Debug.Log("oldest color: " + oldestColor);
-            lastColor[clickCount%4] = 3;
-            if (oldestColor == 1){
-                redCount--;
-            }
-            else if (oldestColor == 2){
-                yellowCount--;
-            }
-            else if (oldestColor == 3){
-                blueCount--;
-            }
-        }
-        clickCount++;
-        Debug.Log("Red count: " + redCount + "Blue count: " + blueCount + "Yellow count: " + yellowCount);
+        UpdateLastColor(3);
         UpdateResultColor();
+    }
+
+    private void UpdateLastColor(int color)
+    {
+        lastColor[lastIndex] = color; 
+        lastIndex = (lastIndex + 1) % 4;
+        clickCount++;
     }
 
     private void UpdateResultColor()
     {
         Debug.Log("Updating result color...");
-        // Call GetResultColor to determine the correct color
         Sprite colorSprite = GetResultColor();
-
-        // Render the resulting color sprite
         resultRenderer.sprite = colorSprite;
     }
 
     private Sprite GetResultColor()
+{
+    int[] colorCounts = new int[4];
+    for (int i = 0; i < 4; i++)
     {
-        Debug.Log("Getting result color...");
-        // Determine the resulting color based on the number of red, yellow, and blue picks
-        if( redCount > 1 && blueCount > 1 && yellowCount > 1){
-            return Resources.Load<Sprite>("B");
-        }
-        else if (redCount == 4)
-        {
-            Debug.Log("Result color: Red");
-            return Resources.Load<Sprite>("R");
-        }
-        else if (redCount == 3 && yellowCount == 1)
-        {
-            Debug.Log("Result color: Blood Orange");
-            return Resources.Load<Sprite>("BO");
-        }
-        else if (redCount == 2 && yellowCount == 2)
-        {
-            Debug.Log("Result color: Orange");
-            return Resources.Load<Sprite>("O");
-        }
-        else if (redCount == 1 && yellowCount == 3)
-        {
-            Debug.Log("Result color: Sunshine Yellow");
-            return Resources.Load<Sprite>("SY");
-        }
-        else if (yellowCount == 4)
-        {
-            Debug.Log("Result color: Yellow");
-            return Resources.Load<Sprite>("Y");
-        }
-        else if (yellowCount == 3 && blueCount == 1)
-        {
-            Debug.Log("Result color: Lime Green");
-            return Resources.Load<Sprite>("SG");
-        }
-        else if (yellowCount == 2 && blueCount == 2)
-        {
-            Debug.Log("Result color: Green");
-            return Resources.Load<Sprite>("G");
-        }
-        else if (yellowCount == 1 && blueCount == 3)
-        {
-            Debug.Log("Result color: Sea Blue");
-            return Resources.Load<Sprite>("SB");
-        }
-        else if (blueCount == 4)
-        {
-            Debug.Log("Result color: Bright Blue");
-            return Resources.Load<Sprite>("BB");
-        }
-        else if (blueCount == 3 && redCount == 1) 
-        {
-            Debug.Log("Result color: Navy Blue");
-            return Resources.Load<Sprite>("NB");
-        }
-        else if (blueCount == 2 && redCount == 2) 
-        {
-            Debug.Log("Result color: Purple");
-            return Resources.Load<Sprite>("P");
-        }
-        else if (blueCount == 1 && redCount == 3) 
-        {
-            Debug.Log("Result color: Violet");
-            return Resources.Load<Sprite>("V");
-        }
-        else
-        {
-            Debug.Log("Result color: Default");
-            return Resources.Load<Sprite>("B");
-        }
+        colorCounts[lastColor[i] - 1]++;
     }
+
+    bool hasRed = colorCounts[0] > 0;
+    bool hasYellow = colorCounts[1] > 0;
+    bool hasBlue = colorCounts[2] > 0;
+
+    if (hasRed && hasYellow && hasBlue)
+    {
+        return Resources.Load<Sprite>("B");
+    }
+    else if (hasRed && colorCounts[0] == 4)
+    {
+        return Resources.Load<Sprite>("R");
+    }
+    else if (hasRed && colorCounts[0] == 3 && colorCounts[1] == 1)
+    {
+        return Resources.Load<Sprite>("BO");
+    }
+    else if (hasRed && colorCounts[0] == 2 && colorCounts[1] == 2)
+    {
+        return Resources.Load<Sprite>("O");
+    }
+    else if (hasRed && colorCounts[0] == 1 && colorCounts[1] == 3)
+    {
+        return Resources.Load<Sprite>("SY");
+    }
+    else if (hasYellow && colorCounts[1] == 4)
+    {
+        return Resources.Load<Sprite>("Y");
+    }
+    else if (hasYellow && colorCounts[1] == 3 && colorCounts[2] == 1)
+    {
+        return Resources.Load<Sprite>("SG");
+    }
+    else if (hasYellow && colorCounts[1] == 2 && colorCounts[2] == 2)
+    {
+        return Resources.Load<Sprite>("G");
+    }
+    else if (hasYellow && colorCounts[1] == 1 && colorCounts[2] == 3)
+    {
+        return Resources.Load<Sprite>("SB");
+    }
+    else if (hasBlue && colorCounts[2] == 4)
+    {
+        return Resources.Load<Sprite>("BB");
+    }
+    else if (hasBlue && colorCounts[2] == 3 && colorCounts[0] == 1)
+    {
+        return Resources.Load<Sprite>("NB");
+    }
+    else if (hasBlue && colorCounts[2] == 2 && colorCounts[0] == 2)
+    {
+        return Resources.Load<Sprite>("P");
+    }
+    else if (hasBlue && colorCounts[2] == 1 && colorCounts[0] == 3)
+    {
+        return Resources.Load<Sprite>("V");
+    }
+    else
+    {
+        return Resources.Load<Sprite>("DefaultSprite");
+    }
+}
 }
